06371fc59d6a1c8e617b5dec6bb9cb6676073ff5
kikuch1 <atera376zaw@gmail.com>
2024-03-28 16:25:12 +0900

    Add ToDo Apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useState } from "react"
+
+type Task = {
+  id: string,
+  title: string,
+  completed: boolean
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [input, setInput] = useState("")
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const task: Task = {
+      id: crypto.randomUUID(),
+      title: input,
+      completed: false
+    }
+
+    setTasks([...tasks, task])
+    setInput("")
+  }
+
+  function handleCheckboxChange(event, task: Task) {
+    console.log("change")
+    setTasks(tasks.map(t => {
+      if (t.id === task.id) {
+        return {
+          ...t,
+          completed: event.target.value,
+        }
+      }
+      return t
+    }))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>ToDoアプリ</h1>
+
+      {tasks.length > 0 ?
+        <>
+          <h2>My Tasks</h2>
+          <ul>
+            {tasks.map(task => (
+              <li key={task.id}>
+                <input type="checkbox" checked={task.completed} onChange={(e) => handleCheckboxChange(e, task)}></input>
+                {task.completed ? <s>{task.title}</s> : task.title}
+              </li>
+            ))}
+          </ul>
+        </> : <p>タスクを追加してください！</p>}
+
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <input type="text" value={input} onChange={(event) => setInput(event.target.value)}></input>
+        <button type="submit">Button</button>
+      </form>
     </>
   )
 }
